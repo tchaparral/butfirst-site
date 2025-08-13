cd9a73d1c5bbc5a51faa51d51d2da8c5c385fa6a
chaparral <chaparral@DESKTOP-U78RO1L.localdomain>
2025-08-13 12:17:20 -0300

    feat: syntax contentlayer.config.ts

--- .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-CZEJNBXP.mjs
+++ .contentlayer/.cache/v0.3.4/compiled-contentlayer-config-CZEJNBXP.mjs
@@ -1,10 +1,8 @@
 // contentlayer.config.ts
-
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
-import rehypeHighlight from "rehype-highlight"; // ou rehypePrettyCode, se preferir
+import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
-
-export const Post = defineDocumentType(() => ({
+var Post = defineDocumentType(() => ({
   name: "Post",
   filePathPattern: `**/*.mdx`,
   contentType: "mdx",
@@ -12,22 +10,25 @@ export const Post = defineDocumentType(() => ({
     title: { type: "string", required: true },
     description: { type: "string", required: true },
     date: { type: "date", required: true },
-    image: { type: "string", required: true },
+    image: { type: "string", required: true }
   },
   computedFields: {
     slug: {
       type: "string",
-      resolve: (post) =>
-        post._raw.sourceFileName.replace(/\.mdx$/, ""),
-    },
-  },
+      resolve: (post) => post._raw.sourceFileName.replace(/\.mdx$/, "")
+    }
+  }
 }));
-
-export default makeSource({
+var contentlayer_config_default = makeSource({
   contentDirPath: "content/blog",
   documentTypes: [Post],
   mdx: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypeHighlight as any],
-  },
+    rehypePlugins: [rehypeHighlight]
+  }
 });
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-CZEJNBXP.mjs.map
